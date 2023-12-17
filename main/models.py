9bab90f395a2a73df5bc9429d53c4c8aa5ff1087
Sergey <seglapinofff@gmail.com>
2023-12-17 23:05:41 +0300

    init

--- main/models.py
+++ main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='название')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='цена')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
